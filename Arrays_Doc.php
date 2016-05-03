9f105c5500d3cdf2303a96404a10e20e78213ead
Xuanyu Liu <sunflowerxuanyu@gmail.com>
2016-05-03 01:48:53 -0400

    add php_array_doc.md

--- Arrays_Doc.php
+++ Arrays_Doc.php
@@ -1,4 +1,16 @@
-<?php
-$input_array = array("FirSt" => 1, "SecOnd" => 4);
-print_r(array_change_key_case($input_array, CASE_UPPER));
-?>
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+    <meta charset="UTF-8">
+    <title>Arrays_Learning_Doc</title>
+</head>
+
+<body>
+    <?php
+        $input_array = array("FirSt" => 1, "SecOnd" => 4);
+        print_r(array_change_key_case($input_array, CASE_UPPER));
+        print_r(array_change_key_case($input_array, CASE_LOWER));
+    ?>
+
+</body>
+</html>
---
+++ PHP_Doc.md
@@ -0,0 +1,17 @@
+When converting to **boolean**, the following values are considered **FALSE**:
+
+1. the boolean FALSE itself
+
+2. the integer 0 (zero)
+
+3. the float 0.0 (zero)
+
+4. the empty string, and the string "0"
+
+5. an array with zero elements
+
+6. an object with zero member variables (PHP 4 only)
+
+7. the special type NULL (including unset variables)
+
+8. SimpleXML objects created from empty tags
